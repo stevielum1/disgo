f9d85ce31fe4465682a9d15396e1ed503bc95da1
Steven Lum <stevielum1@gmail.com>
2018-08-07 13:31:08 -0700

    Add react-router-dom and app component

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = ({ store }) => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
--- frontend/disgo.jsx
+++ frontend/disgo.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './root';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  ReactDOM.render(<Root />, root);
+  ReactDOM.render(<Root store={store} />, root);
 })
