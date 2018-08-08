39471c68c71c47f1a25d81bc329fd14403303843
Steven Lum <stevielum1@gmail.com>
2018-08-07 17:57:39 -0700

    Bootstrap current_user

--- frontend/disgo.jsx
+++ frontend/disgo.jsx
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
+//TEST
+import { logout } from './actions/session_actions';
+//END TEST
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  const store = configureStore();
+
+  let store;
+    if (window.currentUser) {
+      const preloadedState = {
+        session: { id: window.currentUser.id },
+        entities: {
+          users: { [window.currentUser.id]: window.currentUser }
+        }
+      };
+      store = configureStore(preloadedState);
+      delete window.currentUser;
+    } else {
+      store = configureStore();
+    }
 
   //TEST
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.logout = logout;
   //END TEST
 
   ReactDOM.render(<Root store={store} />, root);
