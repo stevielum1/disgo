c5e1ea0f4a6c4a6a905c58d44ec87c0bd8dce82a
Steven Lum <stevielum1@gmail.com>
2018-08-09 22:01:27 -0700

    Add more styling to create server form

--- frontend/components/app/server/create_server_form.jsx
+++ frontend/components/app/server/create_server_form.jsx
@@ -39,6 +39,7 @@ class CreateServerForm extends React.Component {
 
   render() {
     const { errors } = this.props;
+    const photoFileName = this.state.photoFile ? `Uploaded ${this.state.photoFile.name}` : "";
     return (
       <div className="create-server-form-container">
         <form onSubmit={this.handleSubmit}>
@@ -51,20 +52,34 @@ class CreateServerForm extends React.Component {
           <p>
             By creating a server, you will have access to free  <del>voice and</del> text chat to use amongst your friends.
           </p>
-          <label>
-            <p>Server Name</p>
+          <div className="server-input-section">
+            <label>
+              <p>Server Name</p>
+              <input
+                type="text"
+                onChange={this.handleInput}
+                value={this.state.name}
+                placeholder="Enter a server name"
+                autoFocus="true" />
+            </label>
+            <label
+              className="server-photo-input-label"
+              htmlFor="server-photo-input">
+              <div className="server-photo-input-placeholder">
+                <p>Change</p>
+                <p>Icon</p>
+              </div>
+            </label>
             <input
-              type="text"
-              onChange={this.handleInput}
-              value={this.state.name}
-              placeholder="Enter a server name"
-              autoFocus="true" />
-          </label>
-          <input
-            type="file"
-            onChange={this.handleFile}
-            accept="image/*" />
-          <button>Create</button>
+              type="file"
+              id="server-photo-input"
+              onChange={this.handleFile}
+              accept="image/*" />
+          </div>
+          <div className="server-input-footer">
+            <div className="server-photo-filename">{photoFileName}</div>
+            <button>Create</button>
+          </div>
         </form>
       </div>
     )
