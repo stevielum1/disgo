7fac4b88328addbf3b895fd66e5d72c649b3f5e3
Steven Lum <stevielum1@gmail.com>
2018-08-14 18:05:36 -0700

    Remove commented code

--- app/jobs/message_creation_event_broadcast_job.rb
+++ app/jobs/message_creation_event_broadcast_job.rb
@@ -17,18 +17,4 @@ class MessageCreationEventBroadcastJob < ApplicationJob
         type: type
       )
   end
-  # def perform(message)
-  #   ActionCable
-  #     .server
-  #     .broadcast(
-  #       "chat_channel_#{message.channel_id}",
-  #       id: message.id,
-  #       content: message.content,
-  #       authorId: message.author_id,
-  #       channelId: message.channel_id,
-  #       createdAt: message.created_at.strftime('%l:%M %p'),
-  #       updatedAt: message.updated_at.strftime('%l:%M %p'),
-  #       test: "test"
-  #     )
-  # end
 end
