51dcdf5cfeabbedc3689af0d3a6a33480f37a415
Steven Lum <stevielum1@gmail.com>
2018-08-17 12:40:58 -0700

    Remove and then re add user_id column

--- db/migrate/20180817193943_add_unique_to_user_id_in_sessions.rb
+++ db/migrate/20180817193943_add_unique_to_user_id_in_sessions.rb
@@ -1,5 +1,7 @@
 class AddUniqueToUserIdInSessions < ActiveRecord::Migration[5.2]
   def change
+    remove_column :sessions, :user_id
+    add_column :sessions, :user_id, :integer, null: false
     add_index :sessions, :user_id, unique: true
   end
 end
