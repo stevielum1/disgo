d7c96b82e3837c79e0f85beda4a7c36652263e5c
Steven Lum <stevielum1@gmail.com>
2018-08-17 14:19:26 -0700

    Fixed chat not working after switching channels

--- frontend/components/app/message/message_index.jsx
+++ frontend/components/app/message/message_index.jsx
@@ -13,12 +13,25 @@ class MessageIndex extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.props.messages.length !== prevProps.messages.length) {
+    if (prevProps.messages.length !== this.props.messages.length) {
       this.props.fetchServers();
       if (this.chats) {
-        this.chats.unsubscribe();
+        this.chats.unsubscribe()
       }
       this.createSocket();
+      this.scrollToBottom();
+      return;
+    }
+
+    for(let i = 0; i < prevProps.messages.length || i < this.props.messages.length; i++) {
+      if (prevProps.messages[i].id !== this.props.messages[i].id) {
+        this.props.fetchServers();
+        if (this.chats) {
+          this.chats.unsubscribe()
+        }
+        this.createSocket();
+        break;
+      }
     }
     this.scrollToBottom();
   }
